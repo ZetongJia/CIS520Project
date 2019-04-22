0a02b0e66b07b38d79ef690827f0b78c28abd986
ZetongJia <zetongj@seas.upenn.edu>
2019-04-22 17:53:10 -0400

    Update segmentCluster.m

--- segmentCluster.m
+++ segmentCluster.m
@@ -3,19 +3,20 @@ close all
 
 k = 30;
 
-idx = kmeans(featureMatrix,k,'Replicates',5);
+%each row of frequencyMatrix is a segment, each column represents a feature
+%variable
+idx = kmeans(frequencyMatrix,k,'Replicates',5);
 
+%shapeNum is the information about how many segments each image have [1*x dim]
+idx_splitted = mat2cell(idx',1, shapeNum);
 
-idx_splitted = num2cell(idx,1);
-
-numPictures = size(idx_splitted);
-
-% freq_mat = cell(1, numPictures);
+[~, numPictures] = size(shapeNum);
+ 
 freq_mat = zeros(k, numPictures);
 
 for i = 1:numPictures
     segment_vector = idx_splitted{i};
-    a = unique(segment_vector);
+    a = unique(segment_vector');
     out = [a,histc(segment_vector(:),a)];
     freq_mat(out(:,1),i) = out(:,2);
 end
