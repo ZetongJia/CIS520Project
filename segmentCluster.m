38fbb24df55edffd352ce62be1d17e580a3454f6
Zetong Jia <tina_jia@vagelos-ve506-0197.apn.wlan.upenn.edu>
2019-04-24 21:17:55 -0400

    LDA and ACC

--- segmentCluster.m
+++ segmentCluster.m
@@ -1,16 +1,12 @@
-clear
-close all
-
-k = 30;
+k = 200;
 
 %each row of frequencyMatrix is a segment, each column represents a feature
 %variable
-idx = kmeans(frequencyMatrix,k,'Replicates',5);
-
+idx = kmeans(frequencyMatrix,k,'Replicates',1, 'MaxIter', 500);
 %shapeNum is the information about how many segments each image have [1*x dim]
-idx_splitted = mat2cell(idx',1, shapeNum);
+idx_splitted = mat2cell(idx',1, shapeNum3);
 
-[~, numPictures] = size(shapeNum);
+[~, numPictures] = size(shapeNum3);
  
 freq_mat = zeros(k, numPictures);
 
